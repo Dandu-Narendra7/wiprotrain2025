88d60210d3b9a77dad3d35c5d1bb10457d11fbae
Dandu-Narendra7 <narendradandu6@gmail.com>
2025-08-11 13:57:12 +0530

    adding work files

---
+++ 09-08-2025/uber/src/main/java/com/wipro/uber/util/GpayApplication.java
@@ -0,0 +1,11 @@
+package com.wipro.uber.util;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class GpayApplication {
+	   public static void main(String[] args) {
+	        SpringApplication.run(GpayApplication.class, args);
+	    }
+
+}
